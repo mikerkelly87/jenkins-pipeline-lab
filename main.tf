f638affb92b8f2c203b3cabe2730641be092eb19
mikerkelly87 <mikerkelly87@gmail.com>
2023-07-18 00:58:33 -0500

    updating main.tf to fix internet access

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Setup the AWS provider
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_files = ["/home/user/.aws/credentials"]
+  shared_credentials_files = ["/home/user/.aws/credentials"] # REPLACE ME
   profile = "customprofile"
 }
 
@@ -22,7 +22,7 @@ resource "aws_internet_gateway" "igw-jenkins" {
 }
 
 # Create the 'route-table-jenkins' route table to send traffic
-# destined for 0.0.0.0/0 to the 'igw-jenkins' Internet Gateway to allow Internet access
+# destined for 0.0.0.0/0 to the 'igw-jenkins' Internet Gateway
 resource "aws_route_table" "route-table-jenkins" {
   vpc_id = aws_vpc.vpc-jenkins.id
 
@@ -30,6 +30,13 @@ resource "aws_route_table" "route-table-jenkins" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw-jenkins.id
   }
+}
+
+# Create the route-jenkins route for the Internet Gateway to allow Internet access
+resource "aws_route" "route-jenkins" {
+  route_table_id = aws_route_table.route-table-jenkins.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw-jenkins.id
 }
 
 # Associate the 'route-table-jenkins' route table with the 'jenkins-subnet' subnet
@@ -52,15 +59,22 @@ resource "aws_security_group" "security-group-jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Create the 'jenkins-app-test' EC2 instance with the preceeding resources
 # and create the '/home/ec2-user/hello.txt' file with the contents "Hello, World!"
 resource "aws_instance" "jenkins-app-test" {
-  ami           = "ami-xxxxxxxxxxxxxxxxx"
+  ami           = "ami-xxxxxxxxxxxxxxxxx" # REPLACE ME
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.jenkins-subnet.id
-  key_name      = "your_keypair"
+  key_name      = "your_keypair" # REPLACE ME
   associate_public_ip_address = true
 
   vpc_security_group_ids = [aws_security_group.security-group-jenkins.id]
